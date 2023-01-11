a6adace9d597bb350b6eb11fe556402a5144fefe
igorevi4oleg <105513065+igorevi4oleg@users.noreply.github.com>
2023-01-11 16:42:28 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React from "react";
-import Button from "./Button";
-import TimeShow from "./TimeShow";
+/* import Button from "./Button"; */
+/* import '../../src/Bithday.css' */
+/* import '../../src/App.css' */
+import '../../src/Converter.css'
+/* import TimeShow from "./TimeShow"; */
+/* import Birthday from "./Bithday" */
+import ExchangeRates from './Converter'
+
+
 
 
 
@@ -8,9 +15,15 @@ function App() {
 
   return(
   <>
-  <Button />
-  <div>Hello!</div>
-  <div><TimeShow /></div>
+  {/*  <Button /> */}
+
+  {/* <div className="greet-div">Hello!</div>
+  <div className = "timeNow">The time is now<TimeShow /></div> */}
+
+  {/* <div id="app"><div className="container"><Birthday /></div></div> */}
+
+  <div id="app"><ExchangeRates /></div>
+
   </>
   
   )
